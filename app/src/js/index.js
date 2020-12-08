d10e0f1e39f3755b918747453b885d5dea80edb6
Willian Campos <williancampos1995@gmail.com>
2020-12-08 15:02:14 -0300

    WIP JS cards pt3.

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -4,7 +4,38 @@ function getProducts(url) {
   
   function addCards(data) {
     data.products.map((product) => {
-      console.log(product);
+        // remove this later
+        console.log(product);
+
+        const container = document.getElementsByClassName('product-grid');
+        //Constructing card content
+
+        const content = `
+        <div class="product-card">
+            <div class="image-container">
+                <img class="product-image" src="${product.image}" alt="product image"/>
+            </div>
+
+            <div class="product-details">
+                <h1>${product.name}</h1>
+
+                <p>${product.description}</p>
+            </div>
+
+            <div class="product-price">
+                <p>De: R$${product.oldPrice}</p>
+                <h4>Por: R$${product.price}</h4>
+                <p>ou ${product.installments.count}x de R$${product.installments.value}</p>
+            </div>
+
+            <button>
+                <span>Comprar</span>
+            </button>
+        </div>
+        `;
+
+        //Appending card element to grid container
+        container.innerHTML += content;
     });
   }
   
@@ -20,44 +51,4 @@ function getProducts(url) {
         console.log("Error: ", err);
       });
   };
-
-
-
-// // Creating card and attaching products data
-// const container = document.getElementById('product-grid');
-
-// productsResult.forEach(product => {
-//     const card = document.createElement('div');
-//     card.classList = 'card-body';
-
-//     //Constructing card content
-
-//     const content = `
-//     <div class="product-card">
-//         <div class="image-container">
-//             <img class="product-image" src="" alt="product image"/>
-//         </div>
-
-//         <div class="product-details">
-//             <h1>Nome do produto</h1>
-
-//             <p>Descrição do produto um pouco maior, com duas linhas ou três, que explica melhor do que o produto se trata.</p>
-//         </div>
-
-//         <div class="product-price">
-//             <p>De: R$23,99</p>
-//             <h4>Por: R$19,99</h4>
-//             <p>ou 2x de R$9,99</p>
-//         </div>
-
-//         <button>
-//             <span>Comprar</span>
-//         </button>
-//     </div>
-//     `;
-
-//     //Appending card element to grid container
-//     container.innerHTML += content;
-
-// })
 
